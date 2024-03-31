e47bea72e9d05068288ae7a810504a7bef8346c1
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2024-03-31 18:08:19 +0700

    Made My Account Page (WIP), alert on failed login, login logout switching button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { hashSync } = require("bcrypt");
 const UserData = require("./database/userdata.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 const StoreMongo = require("connect-mongo");
 const passport = require("passport");
 
@@ -25,6 +26,8 @@ app.use(session({
     }
 }));
 
+app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -35,24 +38,48 @@ app.get("/", (req, res) => {
 })
 
 app.get("/adopt", (req, res) => {
-    res.render("adopt.ejs", {title : "Adopt a cat | Find a cat to adopt"})
+    res.render("adopt.ejs", {title : "Adopt a cat | Find a cat to adopt", loggedin: req.session.loggedin})
 })
 
 app.get("/about", (req, res) => {
-    res.render("about.ejs", {title : "About Cat Protection | Who are Cats Protection"})
+    res.render("about.ejs", {title : "About Cat Protection | Who are Cats Protection",loggedin: req.session.loggedin})
 })
 
 app.get("/what-we-do", (req, res) => {
-    res.render("whatwedo.ejs", {title : "What we do | Helping Cats and Kitten"})
+    res.render("whatwedo.ejs", {title : "What we do | Helping Cats and Kitten",loggedin: req.session.loggedin})
 })
 
 app.get("/help-and-advice", (req, res) => {
-    res.render("help.ejs", {title : "Help and Advice | Expert Cat Care Guide"})
+    res.render("help.ejs", {title : "Help and Advice | Expert Cat Care Guide",loggedin: req.session.loggedin})
 })
 
 app.get("/find-us", (req, res) => {
-    res.render("find.ejs", {title : "Find Us"})
+    res.render("find.ejs", {title : "Find Us",loggedin: req.session.loggedin})
 })
+
+app.get('/myacc', async function(req, res) {
+    if (req.user) {
+        try {
+            const user = await UserData.findOne({ username: req.user.username });
+            if (user) {
+                res.render('myacc.ejs', {
+                    title : "My Account",
+                    loggedin: req.session.loggedin,
+                    user: user,
+                    username: user.username,
+                    email: user.email,
+                    password: user.password
+                });
+            } else {
+                res.send('No user found with that username');
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        res.send('You are not logged in');
+    }
+});
 
 app.get("/register", (req, res) => {
     res.render("register.ejs", {title : "Register"})
@@ -74,11 +101,22 @@ app.post("/register", async (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-    res.render("login.ejs", {title : "Login"})
+    res.render("login.ejs", {title : "Login", loggedin: req.session.loggedin,message: req.flash('error') })
 })
 
-app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
-        res.redirect('/adopt');
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login',failureFlash: 'Wrong username/password, please try again'}), function(req, res) {
+    req.session.loggedin = true;
+    res.redirect('/adopt');
+});
+
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/adopt');
+        }
+    });
 });
 
 app.listen(port, () => {
