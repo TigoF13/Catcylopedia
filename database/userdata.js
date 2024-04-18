78af507e6a4ffe0c8e81a77863b24110608c4342
JasonFTI45 <jason.535220045@stu.untar.ac.id>
2024-04-18 12:04:57 +0700

    Made admin, added add cat feature for admin, added adopted and cats schema, and finished form

--- database/userdata.js
+++ database/userdata.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    adoptedcats: {
+        type: Array,
+        required: false
     }
 });
 
